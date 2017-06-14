7e07ca36dc36692343dc3106ee3956a13f96cd17
vinisns <vinisns.design@gmail.com>
2017-06-14 17:04:16 -0300

    implementacao

--- src/pm_SalesForce/Produto.java
+++ src/pm_SalesForce/Produto.java
@@ -3,5 +3,23 @@ package pm_SalesForce;
 public class Produto {
 	private String descricao;
 	private Categoria categoria;
+	
+	Produto(String desc, Categoria catg){
+		setDescricao(desc);
+		setCategoria(catg);		
+	}	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
